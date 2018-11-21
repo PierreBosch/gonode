1868d49061d640757a85db10cae6be854fb9a7b8
PierreBosch <pierre.mota@sc.senai.br>
2018-11-21 18:24:56 -0200

    Aulas concluidas

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express');
 
 const app = express();
 
+const logMiddleware = (req, res, next) => {
+    console.log(
+        `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}}`
+    );
+
+    return next();
+}
+
 app.get('/', (req, res) => {
-    return res.send("Hello world");
+    return res.send(`Bem-vindo, ${req.query.name}`);
 });
 
-app.get('/nome/:name', (req, res) => {
-    return res.send(`Bem-vindo, ${req.params.name}`);
+app.get('/nome/:name', logMiddleware, (req, res) => {
+    return res.json({
+        message: `Welcome, ${req.params.name}`
+    })
 });
 
 app.listen(3000);
