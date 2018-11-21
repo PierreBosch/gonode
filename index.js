4ede1e74e3c1433237cccc653258517634b69fd3
PierreBosch <pierre.mota@sc.senai.br>
2018-11-21 18:05:24 -0200

    First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send("Hello world");
+});
+
+app.get('/nome/:name', (req, res) => {
+    return res.send(`Bem-vindo, ${req.params.name}`);
+});
+
+app.listen(3000);
