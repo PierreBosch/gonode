20de8dc864119fb6f6a7110d4735dee5bc77e6ab
PierreBosch <pierre.mota@sc.senai.br>
2018-11-27 10:28:58 -0200

    Primeiro modulo

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-const express = require('express');
+const express = require('express')
+const nunjucks = require('nunjucks')
 
-const app = express();
+const app = express()
 
-const logMiddleware = (req, res, next) => {
-    console.log(
-        `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}}`
-    );
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
 
-    return next();
-}
+app.use(express.urlencoded({ extended: false }))
+
+app.set('view engine', 'njk')
+
+const users = ['Diego Fernandes', 'Robson Marques', 'Cleiton Souza']
 
 app.get('/', (req, res) => {
-    return res.send(`Bem-vindo, ${req.query.name}`);
-});
+  return res.render('list', { users })
+})
 
-app.get('/nome/:name', logMiddleware, (req, res) => {
-    return res.json({
-        message: `Welcome, ${req.params.name}`
-    })
-});
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
 
-app.listen(3000);
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  return res.redirect('/')
+})
+
+app.listen(3000)
